f6755e84879d67f4a03d9db373a7737c72ba4c2c
SiamKing <thesiameseelectric@gmail.com>
2017-08-31 19:44:35 -0700

    added more tests to user spec and method to user.js

--- app/services/users/users.spec.js
+++ app/services/users/users.spec.js
@@ -1,5 +1,56 @@
 describe('Users factory', () => {
-  it('has a dummy spec to test 2 + 2', () => {
-    expect().toEqual(4);
+  var Users;
+
+  var userList = [
+    {
+      id: '1',
+      name: 'Jane',
+      role: 'Designer',
+      location: 'New York',
+      twitter: 'gijane'
+    },
+    {
+      id: '2',
+      name: 'Bob',
+      role: 'Developer',
+      location: 'New York',
+      twitter: 'billybob'
+    },
+    {
+      id: '3',
+      name: 'Jim',
+      role: 'Developer',
+      location: 'Chicago',
+      twitter: 'jimbo'
+    },
+    {
+      id: '4',
+      name: 'Bill',
+      role: 'Designer',
+      location: 'LA',
+      twitter: 'dabill'
+    }
+  ];
+
+  // Before each test load our api.users module
+  beforeEach(angular.mock.module('api.users'));
+
+  // Before each test set our injected Users factory (_Users_) to our local Users variable
+  beforeEach(inject((_Users_) => {
+    Users = _Users_;
+  }));
+
+  it('should exist', () => {
+    expect(Users).toBeDefined();
   });
+
+  describe('.all()', () => {
+    it('should exist', () => {
+      expect(Users.all).toBeDefined();
+    });
+
+    it('should return a hard-coded list of users', () => {
+      expect(Users.all()).toEqual(userList);
+    })
+  })
 });
