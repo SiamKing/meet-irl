036b3c1d9cbb343450890849f775b3be09731a71
SiamKing <thesiameseelectric@gmail.com>
2017-09-05 17:30:46 -0700

    added tests and actions for profile

--- app/components/profile/profile.js
+++ app/components/profile/profile.js
@@ -1,23 +1,33 @@
-(() => {
+(function() {
   'use strict';
 
-  angular
-    .module('components.profile', [])
-    .controller('ProfileController', function(resolvedUser) {
-      let vm = this;
-      vm.user = resolvedUser;
+  angular.module('components.profile', [])
+  .controller('ProfileController', function(resolvedUser, Pokemon) {
+    var vm = this;
+    vm.user = resolvedUser;
+
+    Pokemon.findByName(vm.user.pokemon.name)
+    .then(function(result) {
+      vm.user.pokemon.id = result.id;
+      vm.user.pokemon.image = result.sprites.front_default;
+      vm.user.pokemon.type = result.types[0].type.name;
     })
-    .config(['$stateProvider', function($stateProvider) {
-      $stateProvider
-        .state('profile', {
-          url: '/user/:id',
-          templateUrl: 'components/profile/profile.html',
-          controller: 'ProfileController as pc',
-          resolve: {
-            resolvedUser: function(Users, $stateParams) {
-              return Users.findById($stateParams.id);
-            }
+    .catch(function(result) {
+      // Add the default placeholder image
+      vm.user.pokemon.image = 'http://i.imgur.com/HddtBOT.png';
+    });
+  })
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('profile', {
+        url: '/user/:id',
+        templateUrl: 'components/profile/profile.html',
+        controller: 'ProfileController as pc',
+        resolve: {
+          resolvedUser: function(Users, $stateParams) {
+            return Users.findById($stateParams.id);
           }
-        })
-    }])
+        }
+      });
+  });
 })();
--- app/components/profile/profile.spec.js
+++ app/components/profile/profile.spec.js
@@ -1,5 +1,57 @@
 describe('components.profile', function() {
-  var $controller,
+  var $controller, PokemonFactory, $q, $httpBackend;
+  var API = 'http://pokeapi.co/api/v2/pokemon/';
+  var RESPONSE_SUCCESS = {
+    'id': 58,
+    'name': 'growlithe',
+    'sprites': {
+      'front_default': 'http://pokeapi.co/media/sprites/pokemon/58.png'
+    },
+    'types': [{
+      'type': { 'name': 'fire' }
+    }]
+  };
+
+  // Add mocked Pokéapi response
+  var RESPONSE_ERROR = {
+    'detail': 'Not found.'
+  };
+
+  beforeEach(angular.mock.module('ui.router'));
+  beforeEach(angular.mock.module('api.pokemon'));
+  beforeEach(angular.mock.module('components.profile'));
+
+  beforeEach(inject(function(_$controller_, _Pokemon_, _$q_, _$httpBackend_) {
+    $controller = _$controller_;
+    PokemonFactory = _Pokemon_;
+    $q = _$q_;
+    $httpBackend = _$httpBackend_;
+  }));
+
+  describe('ProfileController', function() {
+    var ProfileController, singleUser;
+
+    beforeEach(function() {
+      singleUser = {
+        id: '2',
+        name: 'Bob',
+        role: 'Developer',
+        location: 'New York',
+        twitter: 'billybob',
+        pokemon: { name: 'growlithe' }
+      };
+      ProfileController = $controller('ProfileController', { resolvedUser: singleUser, Pokemon: PokemonFactory });
+    });
+
+    it('should be defined', function() {
+      expect(ProfileController).toBeDefined();
+    });
+  });
+
+  describe('Profile Controller with a valid resolved user and a valid Pokemon', function() {
+    var singleUser, ProfileController;
+
+    beforeEach(function() {
       singleUser = {
         id: '2',
         name: 'Bob',
@@ -9,41 +61,62 @@ describe('components.profile', function() {
         pokemon: { name: 'growlithe' }
       };
 
-  // Load ui.router and our components.profile module which we'll create next
-  beforeEach(angular.mock.module('ui.router'));
-  beforeEach(angular.mock.module('components.profile'));
+      spyOn(PokemonFactory, "findByName").and.callThrough();
 
-  // Inject the $controller service
-  beforeEach(inject(function(_$controller_) {
-    $controller = _$controller_;
-  }));
-
-  describe('ProfileController', function() {
-    var ProfileController, singleUser;
-
-    beforeEach(function() {
-      // Define singleUser and add resolvedUser as a dependency to our controller
-
-      // Create an instance of our controller
-      ProfileController = $controller('ProfileController', { resolvedUser: singleUser });
+      ProfileController = $controller('ProfileController', { resolvedUser: singleUser, Pokemon: PokemonFactory });
     });
 
-    // Verify our controller exists
-    it('should be defined', function() {
-      expect(ProfileController).toBeDefined();
+    it('should set the view model user object to the resolvedUser', function() {
+      expect(ProfileController.user).toEqual(singleUser);
+    });
+
+    it('should call Pokemon.findByName and return a Pokemon object', function() {
+      expect(ProfileController.user.pokemon.id).toBeUndefined();
+      expect(ProfileController.user.pokemon.name).toEqual('growlithe');
+      expect(ProfileController.user.pokemon.image).toBeUndefined();
+      expect(ProfileController.user.pokemon.type).toBeUndefined();
+
+      $httpBackend.whenGET(API + singleUser.pokemon.name).respond(200, $q.when(RESPONSE_SUCCESS));
+      $httpBackend.flush();
+
+      expect(PokemonFactory.findByName).toHaveBeenCalledWith('growlithe');
+      expect(ProfileController.user.pokemon.id).toEqual(58);
+      expect(ProfileController.user.pokemon.name).toEqual('growlithe');
+      expect(ProfileController.user.pokemon.image).toContain('.png');
+      expect(ProfileController.user.pokemon.type).toEqual('fire');
     });
   });
 
-  describe('Profile Controller with a valid resolved user', function() {
-   var ProfileController;
+  // Add our new test
+  describe('Profile Controller with a valid resolved user and an invalid Pokemon', function () {
+    var singleUser, ProfileController;
 
-   beforeEach(function() {
-     // Add the valid user as our resolved dependency
-     ProfileController = $controller('ProfileController', { resolvedUser: singleUser });
-   });
+    beforeEach(function() {
+      // Update Pokémon name
+      singleUser = {
+        id: '2',
+        name: 'Bob',
+        role: 'Developer',
+        location: 'New York',
+        twitter: 'billybob',
+        pokemon: { name: 'godzilla' }
+      };
 
-   it('should set the view model user object to the resolvedUser', function() {
-     expect(ProfileController.user).toEqual(singleUser);
-   });
- });
+      spyOn(PokemonFactory, "findByName").and.callThrough();
+
+      ProfileController = $controller('ProfileController', { resolvedUser: singleUser, Pokemon: PokemonFactory });
+    });
+
+    it('should call Pokemon.findByName and default to a placeholder image', function() {
+      expect(ProfileController.user.pokemon.image).toBeUndefined();
+
+      // Declare the endpoint we expect our service to hit and provide it with our mocked return values
+      $httpBackend.whenGET(API + singleUser.pokemon.name).respond(404, Common.silenceUncaughtInPromise($q.reject(RESPONSE_ERROR)));
+      $httpBackend.flush();
+
+      // Add expectation that our image will be set to a placeholder image
+      expect(PokemonFactory.findByName).toHaveBeenCalledWith('godzilla');
+      expect(ProfileController.user.pokemon.image).toEqual('http://i.imgur.com/HddtBOT.png');
+    });
+  });
 });
